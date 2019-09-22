bf9d23e407357da19bea740f4757c1499a6043b5
Christina Gonzalez <cmgamway@gmail.com>
2019-09-22 05:12:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,14 @@
 class EmailAddressParser
-  attr_accessor :email_address
+  attr_accessor :name, :csv_emails
 
-  def initialize(email_addresses)
-    @email_address = email_addresses
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
   end
 
   def parse
-    email_address_array = @email_address.split(/[,]/).uniq
-    email_address_array.reject! {|element| element.empty?}
-    email_address_array
+    csv_emails.split(',')
+    address.split(',')
+  end
+  .flatten.uniq
   end
 end
